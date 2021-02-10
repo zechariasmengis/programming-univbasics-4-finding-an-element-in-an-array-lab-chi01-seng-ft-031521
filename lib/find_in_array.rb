27a565b84769fc8d29f79949d6845a9a2afa1507
Zecharias Mengis <zmengis@gmail.com>
2021-02-10 17:36:56 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,10 @@
+require 'pry'
 
 def find_element_index(array, value_to_find)
   counter = 0
   while counter < array.length do
     if array[counter] == value_to_find
+      binding.pry
       puts array.index(counter)
     else
       counter += 1
